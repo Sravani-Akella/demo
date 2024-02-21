4d47443aefce851b5f05b9e8cdf26a8b717d72df
sravani <sravani170897@gmail.com>
2024-02-21 20:20:43 +0530

    first commit

---
+++ src/main/java/demo/Shyam.java
@@ -0,0 +1,19 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Shyam {
+	
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html");
+	}
+	
+	
+
+}
